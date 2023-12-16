a91e428ebe5f12d463c5995d1222602beb9c9d0e
dmitriy <linev@programmer.net>
2023-12-16 13:47:47 +0200

    Refactor 12/1, add solution for /2 which sadly doesn't work

--- src/day_12.rs
+++ src/day_12.rs
@@ -35,7 +35,7 @@ fn get_broken_springs(springs: &Vec<char>) -> Vec<i32> {
     result
 }
 
-fn generate_possible_springs(pattern: &Vec<char>) -> LinkedList<Vec<char>> {
+fn generate_all_possible_springs(pattern: &Vec<char>) -> LinkedList<Vec<char>> {
     let mut result = LinkedList::new();
     result.push_back(pattern.to_owned());
     while result.front().unwrap().iter().any(|c| *c == '?') {
@@ -54,6 +54,17 @@ fn generate_possible_springs(pattern: &Vec<char>) -> LinkedList<Vec<char>> {
     result
 }
 
+fn generate_possible_springs(pattern: &Vec<char>, damaged: &Vec<i32>) -> Vec<Vec<char>> {
+    generate_all_possible_springs(&pattern)
+        .iter()
+        .filter(|config| {
+            let pattern = get_broken_springs(config);
+            pattern == *damaged
+        })
+        .map(|config| config.clone())
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -63,9 +74,9 @@ mod tests {
     }
 
     #[test]
-    fn generate_possible_springs_test() {
+    fn generate_all_possible_springs_test() {
         let pattern: Vec<char> = str_to_chars("??...#...#...");
-        let springs = generate_possible_springs(&pattern);
+        let springs = generate_all_possible_springs(&pattern);
         assert_eq!(4, springs.len());
         assert!(springs.contains(&str_to_chars(".....#...#...")));
         assert!(springs.contains(&str_to_chars("#....#...#...")));
@@ -87,15 +98,8 @@ pub mod part1 {
     use super::*;
 
     fn solve_spring(s: &Springs) -> u64 {
-        let mut result = 0;
-        let configs = generate_possible_springs(&s.statuses);
-        for config in configs.iter() {
-            let pattern = get_broken_springs(&config);
-            if pattern == s.damaged {
-                result += 1;
-            }
-        }
-        result
+        let configs = generate_possible_springs(&s.statuses, &s.damaged);
+        return configs.len() as u64;
     }
 
     fn solve(springs: &Vec<Springs>) -> u64 {
@@ -150,16 +154,85 @@ pub mod part1 {
 pub mod part2 {
     use super::*;
 
-    fn fold(s: &mut Springs) {
-        let mut statuses = Vec::new();
-        for _ in 0..4 {
-            statuses.append(&mut s.statuses.clone());
-            statuses.push('?');
-        }
-        statuses.append(&mut s.statuses);
-        s.statuses = statuses;
+    fn get_combinations(configs: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let mut result = Vec::new();
 
-        s.damaged = s.damaged.repeat(5);
+        // well...
+        for i1 in 0..configs.len() {
+            for i2 in 0..configs.len() {
+                for i3 in 0..configs.len() {
+                    for i4 in 0..configs.len() {
+                        for i5 in 0..configs.len() {
+                            let mut case = Vec::new();
+                            case.append(&mut configs[i1].clone());
+                            case.push('?');
+                            case.append(&mut configs[i2].clone());
+                            case.push('?');
+                            case.append(&mut configs[i3].clone());
+                            case.push('?');
+                            case.append(&mut configs[i4].clone());
+                            case.push('?');
+                            case.append(&mut configs[i5].clone());
+
+                            result.push(case);
+                        }
+                    }
+                }
+            }
+        }
+
+        result
+    }
+
+    fn solve_spring(s: &Springs) -> u64 {
+        let configs = generate_possible_springs(&s.statuses, &s.damaged);
+        println!("{:?}", configs.len());
+        let configs = get_combinations(&configs);
+        println!("{:?}", configs.len());
+
+        let damaged = s.damaged.repeat(5);
+
+        let mut result = 0;
+        for pattern in configs.iter() {
+            let possible_configs = generate_possible_springs(pattern, &damaged);
+            for c in possible_configs.iter() {
+                let current_damaged = get_broken_springs(c);
+                if current_damaged == damaged {
+                    result += 1;
+                }
+            }
+        }
+
+        result
+    }
+
+    fn solve(springs: &Vec<Springs>) -> u64 {
+        let mut result = 0;
+        for s in springs.iter() {
+            result += solve_spring(&s);
+            break;
+        }
+        result
+    }
+
+    pub struct Solver {}
+    impl crate::aoc::Solver for Solver {
+        fn solve(file_name: &str) -> String {
+            let mut springs = Vec::new();
+            for line in std::fs::read_to_string(file_name).unwrap().lines() {
+                let s = Springs::parse(line);
+                springs.push(s);
+            }
+            solve(&springs).to_string()
+        }
+
+        fn day() -> i32 {
+            12
+        }
+
+        fn part() -> i32 {
+            2
+        }
     }
 
     #[cfg(test)]
@@ -167,20 +240,10 @@ pub mod part2 {
         use super::*;
 
         #[test]
-        fn fold_test() {
-            let mut s = Springs::parse("???.### 1,1,3");
-            fold(&mut s);
-            assert_eq!(
-                Springs::parse("???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3"),
-                s
-            );
-
-            let mut s = Springs::parse(".??..??...?##. 1,1,3");
-            fold(&mut s);
-            assert_eq!(
-                Springs::parse(".??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##. 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3"),
-                s
-            );
+        fn solve_spring_test() {
+            // assert_eq!(1, solve_spring(&Springs::parse("???.### 1,1,3")));
+            assert_eq!(16384, solve_spring(&Springs::parse(".??..??...?##. 1,1,3")));
+            assert_eq!(16, solve_spring(&Springs::parse("????.#...#... 4,1,1")));
         }
     }
 }
