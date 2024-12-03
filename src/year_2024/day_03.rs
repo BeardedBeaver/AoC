15f882e7b92a262399a878a34f6c943a63b80e35
Dmitriy <linev@programmer.net>
2024-12-03 10:03:50 +0200

    Skeleton for day 3

---
+++ src/year_2024/day_03.rs
@@ -0,0 +1,53 @@
+pub mod part1 {
+    pub struct Puzzle {}
+    impl aoc::Puzzle for Puzzle {
+        fn solve(input_file_name: &str) -> String {
+            "".to_string()
+        }
+
+        fn day() -> i32 {
+            3
+        }
+
+        fn part() -> i32 {
+            1
+        }
+
+        fn year() -> i32 {
+            2024
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn test() {}
+    }
+}
+
+pub mod part2 {
+    pub struct Puzzle {}
+    impl aoc::Puzzle for Puzzle {
+        fn solve(input_file_name: &str) -> String {
+            return "".to_string();
+        }
+
+        fn day() -> i32 {
+            3
+        }
+
+        fn part() -> i32 {
+            2
+        }
+
+        fn year() -> i32 {
+            2024
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn test() {}
+    }
+}
