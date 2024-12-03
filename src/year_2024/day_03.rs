7a4172e5982affccb1237ce0cb6a7c60e1edded3
Dmitriy <linev@programmer.net>
2024-12-03 14:16:51 +0200

    Cleanup

--- src/year_2024/day_03.rs
+++ src/year_2024/day_03.rs
@@ -1,32 +1,4 @@
-use aoc::parse_or_panic;
-
-fn get_mul_commands(line: &str) -> Vec<(i64, i64)> {
-    let re = regex::Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
-    let captures = re.captures_iter(line);
-    let mut result = Vec::new();
-    for cap in captures {
-        result.push((parse_or_panic(&cap[1]), parse_or_panic(&cap[2])));
-    }
-    result
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn get_mul_commands_test() {
-        let line = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        let result = super::get_mul_commands(line);
-        assert_eq!(result, vec![(2, 4), (5, 5), (11, 8), (8, 5)]);
-
-        let line = "xmul(2,4567)";
-        let result = super::get_mul_commands(line);
-        assert!(result.is_empty());
-    }
-}
-
 pub mod part1 {
-    use super::get_mul_commands;
-
     pub struct Puzzle {}
     impl aoc::Puzzle for Puzzle {
         fn solve(input_file_name: &str) -> String {
@@ -53,10 +25,28 @@ pub mod part1 {
         }
     }
 
+    fn get_mul_commands(line: &str) -> Vec<(i64, i64)> {
+        let re = regex::Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+        let captures = re.captures_iter(line);
+        let mut result = Vec::new();
+        for cap in captures {
+            result.push((aoc::parse_or_panic(&cap[1]), aoc::parse_or_panic(&cap[2])));
+        }
+        result
+    }
+
     #[cfg(test)]
     mod tests {
         #[test]
-        fn test() {}
+        fn get_mul_commands_test() {
+            let line = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+            let result = super::get_mul_commands(line);
+            assert_eq!(result, vec![(2, 4), (5, 5), (11, 8), (8, 5)]);
+
+            let line = "xmul(2,4567)";
+            let result = super::get_mul_commands(line);
+            assert!(result.is_empty());
+        }
     }
 }
 
