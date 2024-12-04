0982e750c7918489e75f828bc9f251304dbc3b81
dmitriy <linev@programmer.net>
2024-12-04 21:17:21 +0200

    Add solution template

---
+++ template.rs
@@ -0,0 +1,57 @@
+pub mod part1 {
+    pub struct Puzzle {}
+    impl aoc::Puzzle for Puzzle {
+        fn solve(input_file_name: &str) -> String {
+            "".to_string()
+        }
+
+        fn day() -> i32 {
+            todo!();
+        }
+
+        fn part() -> i32 {
+            todo!();
+        }
+
+        fn year() -> i32 {
+            todo!();
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn test() {
+            todo!();
+        }
+    }
+}
+
+pub mod part2 {
+    pub struct Puzzle {}
+    impl aoc::Puzzle for Puzzle {
+        fn solve(input_file_name: &str) -> String {
+            "".to_string()
+        }
+
+        fn day() -> i32 {
+            todo!();
+        }
+
+        fn part() -> i32 {
+            todo!();
+        }
+
+        fn year() -> i32 {
+            todo!();
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn test() {
+            todo!();
+        }
+    }
+}
