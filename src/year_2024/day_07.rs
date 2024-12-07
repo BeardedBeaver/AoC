8a1346f4561165e3ae8c269b1ab2aae01adbd75d
linev <linev@programmer.net>
2024-12-07 18:09:22 +0200

    Cleanup

--- src/year_2024/day_07.rs
+++ src/year_2024/day_07.rs
@@ -6,6 +6,7 @@ struct Equation {
 
 fn parse_equation(line: &str) -> Equation {
     let parts = line.split(':').collect::<Vec<&str>>();
+    assert_eq!(parts.len(), 2);
     let operands = parts[1].split(" ").filter(|i| !i.is_empty()).collect::<Vec<&str>>();
     Equation {
         value: aoc::parse_or_panic(parts[0]),
@@ -25,73 +26,64 @@ mod tests {
 
     #[test]
     fn test_parse_equation() {
-        let eq = parse_equation("190: 10 19");
         assert_eq!(
-            eq,
+            parse_equation("190: 10 19"),
             Equation {
                 value: 190,
                 operands: vec![10, 19]
             }
         );
-        let eq = parse_equation("3267: 81 40 27");
         assert_eq!(
-            eq,
+            parse_equation("3267: 81 40 27"),
             Equation {
                 value: 3267,
                 operands: vec![81, 40, 27]
             }
         );
-        let eq = parse_equation("83: 17 5");
         assert_eq!(
-            eq,
+            parse_equation("83: 17 5"),
             Equation {
                 value: 83,
                 operands: vec![17, 5]
             }
         );
-        let eq = parse_equation("156: 15 6");
         assert_eq!(
-            eq,
+            parse_equation("156: 15 6"),
             Equation {
                 value: 156,
                 operands: vec![15, 6]
             }
         );
-        let eq = parse_equation("7290: 6 8 6 15");
         assert_eq!(
-            eq,
+            parse_equation("7290: 6 8 6 15"),
             Equation {
                 value: 7290,
                 operands: vec![6, 8, 6, 15]
             }
         );
-        let eq = parse_equation("161011: 16 10 13");
         assert_eq!(
-            eq,
+            parse_equation("161011: 16 10 13"),
             Equation {
                 value: 161011,
                 operands: vec![16, 10, 13]
             }
         );
-        let eq = parse_equation("192: 17 8 14");
         assert_eq!(
-            eq,
+            parse_equation("192: 17 8 14"),
             Equation {
                 value: 192,
                 operands: vec![17, 8, 14]
             }
         );
-        let eq = parse_equation("21037: 9 7 18 13");
         assert_eq!(
-            eq,
+            parse_equation("21037: 9 7 18 13"),
             Equation {
                 value: 21037,
                 operands: vec![9, 7, 18, 13]
             }
         );
-        let eq = parse_equation("292: 11 6 16 20");
         assert_eq!(
-            eq,
+            parse_equation("292: 11 6 16 20"),
             Equation {
                 value: 292,
                 operands: vec![11, 6, 16, 20]
