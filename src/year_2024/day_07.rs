fccf13a0e12878627b476afd47d01130cfbbeffd
linev <linev@programmer.net>
2024-12-07 18:26:05 +0200

    Day 7 part 1 done

--- src/year_2024/day_07.rs
+++ src/year_2024/day_07.rs
@@ -1,7 +1,12 @@
 #[derive(Debug, PartialEq, Eq)]
 struct Equation {
     value: u64,
-    operands: Vec<i64>,
+    operands: Vec<u64>,
+}
+
+enum Operation {
+    Add,
+    Mul,
 }
 
 fn parse_equation(line: &str) -> Equation {
@@ -14,13 +19,40 @@ fn parse_equation(line: &str) -> Equation {
     }
 }
 
-fn is_valid(equation: &Equation) -> bool {
-    false
+fn is_valid(value: u64, operands: &[u64], operation: Operation, target: u64) -> bool {
+    if value > target {
+        return false;
+    }
+    let new_value = match operation {
+        Operation::Add => value + operands[0],
+        Operation::Mul => value * operands[0],
+    };
+
+    if operands.len() == 1 {
+        return new_value == target;
+    }
+
+    is_valid(new_value, &operands[1..], Operation::Add, target)
+        || is_valid(new_value, &operands[1..], Operation::Mul, target)
+}
+
+fn is_valid_equation(equation: &Equation) -> bool {
+    is_valid(
+        equation.operands[0],
+        &equation.operands[1..],
+        Operation::Add,
+        equation.value,
+    ) || is_valid(
+        equation.operands[0],
+        &equation.operands[1..],
+        Operation::Mul,
+        equation.value,
+    )
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day_07::is_valid;
+    use crate::day_07::is_valid_equation;
 
     use super::{parse_equation, Equation};
 
@@ -97,60 +129,60 @@ mod tests {
             value: 190,
             operands: vec![10, 19],
         };
-        assert_eq!(is_valid(&eq), true);
+        assert_eq!(is_valid_equation(&eq), true);
 
         let eq = Equation {
             value: 3267,
             operands: vec![81, 40, 27],
         };
-        assert_eq!(is_valid(&eq), true);
+        assert_eq!(is_valid_equation(&eq), true);
 
         let eq = Equation {
             value: 83,
             operands: vec![17, 5],
         };
-        assert_eq!(is_valid(&eq), false);
+        assert_eq!(is_valid_equation(&eq), false);
 
         let eq = Equation {
             value: 156,
             operands: vec![15, 6],
         };
-        assert_eq!(is_valid(&eq), false);
+        assert_eq!(is_valid_equation(&eq), false);
 
         let eq = Equation {
             value: 7290,
             operands: vec![6, 8, 6, 15],
         };
-        assert_eq!(is_valid(&eq), false);
+        assert_eq!(is_valid_equation(&eq), false);
 
         let eq = Equation {
             value: 161011,
             operands: vec![16, 10, 13],
         };
-        assert_eq!(is_valid(&eq), false);
+        assert_eq!(is_valid_equation(&eq), false);
 
         let eq = Equation {
             value: 192,
             operands: vec![17, 8, 14],
         };
-        assert_eq!(is_valid(&eq), false);
+        assert_eq!(is_valid_equation(&eq), false);
 
         let eq = Equation {
             value: 21037,
             operands: vec![9, 7, 18, 13],
         };
-        assert_eq!(is_valid(&eq), false);
+        assert_eq!(is_valid_equation(&eq), false);
 
         let eq = Equation {
             value: 292,
             operands: vec![11, 6, 16, 20],
         };
-        assert_eq!(is_valid(&eq), true);
+        assert_eq!(is_valid_equation(&eq), true);
     }
 }
 
 pub mod part1 {
-    use super::{is_valid, parse_equation};
+    use super::{is_valid_equation, parse_equation};
 
     pub struct Puzzle {}
     impl aoc::Puzzle for Puzzle {
@@ -158,23 +190,23 @@ pub mod part1 {
             let mut result = 0;
             for line in std::fs::read_to_string(input_file_name).unwrap().lines() {
                 let eq = parse_equation(line);
-                if is_valid(&eq) {
-                    result += 1;
+                if is_valid_equation(&eq) {
+                    result += eq.value;
                 }
             }
             result.to_string()
         }
 
         fn day() -> i32 {
-            todo!();
+            7
         }
 
         fn part() -> i32 {
-            todo!();
+            2
         }
 
         fn year() -> i32 {
-            todo!();
+            2024
         }
     }
 
