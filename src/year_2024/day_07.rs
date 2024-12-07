2480a9159534deaa1e59cc05742c762c5afa27e4
linev <linev@programmer.net>
2024-12-07 18:43:39 +0200

    2024 Day 7 part 2

--- src/year_2024/day_07.rs
+++ src/year_2024/day_07.rs
@@ -159,7 +159,7 @@ pub mod part1 {
         use crate::day_07::{part1::is_valid_equation, Equation};
 
         #[test]
-        fn test_is_valid() {
+        fn test_is_valid_equation() {
             let eq = Equation {
                 value: 190,
                 operands: vec![10, 19],
@@ -218,28 +218,145 @@ pub mod part1 {
 }
 
 pub mod part2 {
+    use super::{parse_equation, Equation, Operation};
+
+    fn concat(a: u64, b: u64) -> u64 {
+        aoc::parse_or_panic(&(a.to_string() + &b.to_string()))
+    }
+
+    fn is_valid(value: u64, operands: &[u64], operation: Operation, target: u64) -> bool {
+        if value > target {
+            return false;
+        }
+        let new_value = match operation {
+            Operation::Add => value + operands[0],
+            Operation::Mul => value * operands[0],
+            Operation::Concat => concat(value, operands[0]),
+        };
+
+        if operands.len() == 1 {
+            return new_value == target;
+        }
+
+        is_valid(new_value, &operands[1..], Operation::Add, target)
+            || is_valid(new_value, &operands[1..], Operation::Mul, target)
+            || is_valid(new_value, &operands[1..], Operation::Concat, target)
+    }
+
+    fn is_valid_equation(equation: &Equation) -> bool {
+        is_valid(
+            equation.operands[0],
+            &equation.operands[1..],
+            Operation::Add,
+            equation.value,
+        ) || is_valid(
+            equation.operands[0],
+            &equation.operands[1..],
+            Operation::Mul,
+            equation.value,
+        ) || is_valid(
+            equation.operands[0],
+            &equation.operands[1..],
+            Operation::Concat,
+            equation.value,
+        )
+    }
+
     pub struct Puzzle {}
     impl aoc::Puzzle for Puzzle {
         fn solve(input_file_name: &str) -> String {
-            "".to_string()
+            let mut result = 0;
+            for line in std::fs::read_to_string(input_file_name).unwrap().lines() {
+                let eq = parse_equation(line);
+                if is_valid_equation(&eq) {
+                    result += eq.value;
+                }
+            }
+            result.to_string()
         }
 
         fn day() -> i32 {
-            todo!();
+            7
         }
 
         fn part() -> i32 {
-            todo!();
+            2
         }
 
         fn year() -> i32 {
-            todo!();
+            2024
         }
     }
 
     #[cfg(test)]
     mod tests {
+        use crate::day_07::{
+            part2::{concat, is_valid_equation},
+            Equation,
+        };
+
         #[test]
-        fn test() {}
+        fn test_concat() {
+            assert_eq!(concat(15, 6), 156);
+            assert_eq!(concat(17, 8), 178);
+            assert_eq!(concat(123, 456), 123456);
+        }
+
+        #[test]
+        fn test_is_valid_equation() {
+            let eq = Equation {
+                value: 190,
+                operands: vec![10, 19],
+            };
+            assert_eq!(is_valid_equation(&eq), true);
+
+            let eq = Equation {
+                value: 3267,
+                operands: vec![81, 40, 27],
+            };
+            assert_eq!(is_valid_equation(&eq), true);
+
+            let eq = Equation {
+                value: 83,
+                operands: vec![17, 5],
+            };
+            assert_eq!(is_valid_equation(&eq), false);
+
+            let eq = Equation {
+                value: 156,
+                operands: vec![15, 6],
+            };
+            assert_eq!(is_valid_equation(&eq), true);
+
+            let eq = Equation {
+                value: 7290,
+                operands: vec![6, 8, 6, 15],
+            };
+            assert_eq!(is_valid_equation(&eq), true);
+
+            let eq = Equation {
+                value: 161011,
+                operands: vec![16, 10, 13],
+            };
+            assert_eq!(is_valid_equation(&eq), false);
+
+            let eq = Equation {
+                value: 192,
+                operands: vec![17, 8, 14],
+            };
+            assert_eq!(is_valid_equation(&eq), true);
+
+            let eq = Equation {
+                value: 21037,
+                operands: vec![9, 7, 18, 13],
+            };
+            assert_eq!(is_valid_equation(&eq), false);
+
+            let eq = Equation {
+                value: 292,
+                operands: vec![11, 6, 16, 20],
+            };
+            assert_eq!(is_valid_equation(&eq), true);
+        }
     }
 }
